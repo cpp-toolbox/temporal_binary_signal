f097060a56f1281135aa64e07de6525b146a266f
cuppajoeman <ccn@cuppajoeman.com>
2024-10-31 02:59:54 -0400

    feat: improve readability and add simplifying methods

--- temporal_binary_signal.cpp
+++ temporal_binary_signal.cpp
@@ -1,38 +1,37 @@
 #include "temporal_binary_signal.hpp"
 
-TemporalBinarySignal::TemporalBinarySignal() 
-    : current_state(State::off), raw_signal(false) {}
+TemporalBinarySignal::TemporalBinarySignal() : current_state(State::off), raw_signal(false) {}
 
-void TemporalBinarySignal::set_on() {
-    raw_signal = true;
-}
+void TemporalBinarySignal::set_signal(bool value) { raw_signal = value; }
 
-void TemporalBinarySignal::set_off() {
-    raw_signal = false;
-}
+void TemporalBinarySignal::set_on() { raw_signal = true; }
+
+void TemporalBinarySignal::set_off() { raw_signal = false; }
 
 // Update the state based on the previous raw signal state and the current signal state
 void TemporalBinarySignal::process() {
     switch (current_state) {
-        case State::on:
-            if (!raw_signal) {
-                current_state = State::just_off; 
-            }
-            break;
-        case State::off:
-            if (raw_signal) {
-                current_state = State::just_on; 
-            }
-            break;
-        case State::just_on:
-            current_state = raw_signal ? State::on : State::just_off;
-            break;
-        case State::just_off:
-            current_state = not raw_signal ? State::off : State::just_on;
-            break;
+    case State::on:
+        if (!raw_signal) {
+            current_state = State::just_off;
+        }
+        break;
+    case State::off:
+        if (raw_signal) {
+            current_state = State::just_on;
+        }
+        break;
+    case State::just_on:
+        current_state = raw_signal ? State::on : State::just_off;
+        break;
+    case State::just_off:
+        current_state = not raw_signal ? State::off : State::just_on;
+        break;
     }
 }
 
-State TemporalBinarySignal::get_state() const {
-    return current_state;
-}
+State TemporalBinarySignal::get_state() const { return current_state; }
+bool TemporalBinarySignal::is_on() { return current_state == State::on; }
+bool TemporalBinarySignal::is_off() { return current_state == State::off; }
+bool TemporalBinarySignal::is_just_on() { return current_state == State::just_on; }
+bool TemporalBinarySignal::is_just_off() { return current_state == State::just_off; }
--- temporal_binary_signal.hpp
+++ temporal_binary_signal.hpp
@@ -3,26 +3,26 @@
 
 #include <iostream>
 
-enum class State {
-    on,
-    off,
-    just_on,
-    just_off
-};
+enum class State { on, off, just_on, just_off };
 
 class TemporalBinarySignal {
-public:
+  public:
     TemporalBinarySignal();
 
+    void set_signal(bool value);
     void set_on();
     void set_off();
-// process must be called every timestep
+    // process must be called every timestep
     void process();
     State get_state() const;
+    bool is_just_on();
+    bool is_just_off();
+    bool is_on();
+    bool is_off();
 
-private:
+  private:
     State current_state; // Current state of the temporal binary signal
-    bool raw_signal;     
+    bool raw_signal;
 };
 
 #endif // TEMPORAL_BINARY_SIGNAL_HPP
